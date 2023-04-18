df66ed338d9c1a1ba8aa54606439920876edbf7c
meliarisa <127809030+meliarisa@users.noreply.github.com>
2023-04-18 12:13:02 +0800

    Add files via upload

---
+++ sesi5/risaa.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Ultimate v12.5.1 (64 bit)
+MySQL - 10.4.27-MariaDB : Database - dbmahasiswa
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dbmahasiswa` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `dbmahasiswa`;
+
+/*Table structure for table `tbuser` */
+
+DROP TABLE IF EXISTS `tbuser`;
+
+CREATE TABLE `tbuser` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `passkey` varchar(255) DEFAULT NULL,
+  `iduser` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `tbuser` */
+
+insert  into `tbuser`(`id`,`nama`,`email`,`username`,`passkey`,`iduser`) values 
+(1,'melia risa','bonakmelia@gmail.com','risa','7dff3752985c76c69238752080d3ad10','8d01480e04661941f70a43be5f23ae38'),
+(2,'melia risa','bonakmelia@gmail.com','risa','7dff3752985c76c69238752080d3ad10','8d01480e04661941f70a43be5f23ae38');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
